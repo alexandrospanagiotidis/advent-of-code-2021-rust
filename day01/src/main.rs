2f8f24fb02dc5ae707db498aa308589780c54362
Alexandros Panagiotidis <alexandros@panagiotidis.eu>
2021-12-01 12:03:55 +0100

    Add day01 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,24 +1,29 @@
 use std::io::{BufRead, stdin};
 use std::ops::Add;
 
+const WINDOW_SIZE: usize = 3;
+
 fn main() {
     let mut numbers: Vec<i32> = stdin().lock().lines()
         .map(|line| line.expect("Cannot read line"))
         .map(|line| line.parse::<i32>().expect(String::from("Could not convert to number: ").add(&line).as_str()))
         .collect();
 
-    let previous = numbers.drain(..1).collect::<Vec<i32>>();
-    let mut previous = previous.first().unwrap().to_owned();
+    let mut left: usize = 0;
     let mut increased = 0;
 
-    for number in numbers {
-        let difference = number - previous;
+    while left + WINDOW_SIZE < numbers.len() {
+        let previous_window = &numbers[left..left + WINDOW_SIZE];
+        let mut previous_sum: i32 = previous_window.iter().sum();
 
-        if difference > 0 {
+        left += 1;
+
+        let current_window = &numbers[left..left + WINDOW_SIZE];
+        let current_sum: i32 = current_window.iter().sum();
+
+        if current_sum > previous_sum {
             increased += 1;
         }
-
-        previous = number.to_owned();
     }
 
     println!("increased={0:#?}", increased);

80773f8723070f6008d40e1c2a95e4237a37a7f5
Alexandros Panagiotidis <alexandros@panagiotidis.eu>
2021-12-02 13:02:36 +0100

    Refactor day02 a little

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,7 +1,46 @@
+use std::fmt::Debug;
 use std::io::{BufRead, stdin};
-use std::ops::Add;
 
 type ValueType = i32;
+
+#[derive(Clone, Debug)]
+struct Command {
+    command: String,
+    value: ValueType,
+}
+
+impl Command {
+    fn new(line: String) -> Self {
+        let mut tokens = line.split(" ");
+
+        let command = tokens.next().unwrap().to_owned();
+        let value: ValueType = tokens.next()
+            .map(|token| token.parse::<>().unwrap())
+            .unwrap();
+
+        Self {
+            command,
+            value,
+        }
+    }
+}
+
+trait Navigation {
+    fn forward(&mut self, value: ValueType);
+
+    fn up(&mut self, value: ValueType);
+
+    fn down(&mut self, value: ValueType);
+
+    fn execute(&mut self, command: Command) {
+        match command.command.as_str() {
+            "forward" => self.forward(command.value),
+            "up" => self.up(command.value),
+            "down" => self.down(command.value),
+            _ => panic!("Invalid command: {0:?}", command),
+        };
+    }
+}
 
 #[derive(Debug)]
 struct Position {
@@ -16,65 +55,61 @@ struct PositionAim {
     aim: ValueType,
 }
 
+type CommandList = Vec<Command>;
+
 impl Position {
     fn new() -> Self {
         Position {
-            horizontal: 0,
-            depth: 0,
+            horizontal: ValueType::default(),
+            depth: ValueType::default(),
         }
     }
+}
 
+impl Navigation for Position {
     fn forward(&mut self, delta: ValueType) {
         self.horizontal += delta;
     }
 
-    fn down(&mut self, delta: ValueType) {
-        self.depth += delta;
-    }
-
     fn up(&mut self, delta: ValueType) {
         self.depth -= delta;
+    }
+
+    fn down(&mut self, delta: ValueType) {
+        self.depth += delta;
     }
 }
 
 impl PositionAim {
     fn new() -> Self {
         PositionAim {
-            horizontal: 0,
-            depth: 0,
-            aim: 0,
+            horizontal: ValueType::default(),
+            depth: ValueType::default(),
+            aim: ValueType::default(),
         }
     }
+}
 
+impl Navigation for PositionAim {
     fn forward(&mut self, delta: ValueType) {
         self.horizontal += delta;
         self.depth += self.aim * delta;
     }
 
-    fn down(&mut self, delta: ValueType) {
-        self.aim += delta;
-    }
-
     fn up(&mut self, delta: ValueType) {
         self.aim -= delta;
     }
+
+    fn down(&mut self, delta: ValueType) {
+        self.aim += delta;
+    }
 }
 
-fn part1(lines: Vec<String>) -> ValueType {
+fn part1(commands: CommandList) -> ValueType {
     let mut position = Position::new();
 
-    for line in lines {
-        let mut tokens = line.split(" ");
-
-        let command = tokens.next().unwrap();
-        let delta: ValueType = tokens.next().map(|token| token.parse::<ValueType>()).unwrap().unwrap();
-
-        match command {
-            "forward" => position.forward(delta),
-            "up" => position.up(delta),
-            "down" => position.down(delta),
-            _ => panic!("Invalid command: {0:?}", command),
-        };
+    for command in commands {
+        position.execute(command);
     }
 
     println!("position={0:?}", position);
@@ -82,21 +117,11 @@ fn part1(lines: Vec<String>) -> ValueType {
     position.horizontal * position.depth
 }
 
-fn part2(lines: Vec<String>) -> ValueType {
+fn part2(commands: CommandList) -> ValueType {
     let mut position = PositionAim::new();
 
-    for line in lines {
-        let mut tokens = line.split(" ");
-
-        let command = tokens.next().unwrap();
-        let delta: ValueType = tokens.next().map(|token| token.parse::<ValueType>()).unwrap().unwrap();
-
-        match command {
-            "forward" => position.forward(delta),
-            "up" => position.up(delta),
-            "down" => position.down(delta),
-            _ => panic!("Invalid command: {0:?}", command),
-        };
+    for command in commands {
+        position.execute(command);
     }
 
     println!("position={0:?}", position);
@@ -105,17 +130,20 @@ fn part2(lines: Vec<String>) -> ValueType {
 }
 
 fn main() {
-    let lines: Vec<String> = stdin().lock().lines()
+    let commands: CommandList = stdin().lock().lines()
         .map(|line| line.expect("Cannot read line"))
+        .map(|line| Command::new(line))
         .collect();
 
     {
-        let result = part1(lines.clone());
+        let result = part1(commands.clone());
         println!("part1: result={0:?}", result);
+        assert_eq!(result, 1660158);
     }
 
     {
-        let result = part2(lines.clone());
+        let result = part2(commands.clone());
         println!("part2: result={0:?}", result);
+        assert_eq!(result, 1604592846);
     }
 }

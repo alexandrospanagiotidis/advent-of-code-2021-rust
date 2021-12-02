2f2efe78cd01bdf3ebc38163ccff13fb31422fc7
Alexandros Panagiotidis <alexandros@panagiotidis.eu>
2021-12-02 12:23:55 +0100

    Add day02 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -9,6 +9,13 @@ struct Position {
     depth: ValueType,
 }
 
+#[derive(Debug)]
+struct PositionAim {
+    horizontal: ValueType,
+    depth: ValueType,
+    aim: ValueType,
+}
+
 impl Position {
     fn new() -> Self {
         Position {
@@ -30,11 +37,30 @@ impl Position {
     }
 }
 
-fn main() {
-    let lines: Vec<String> = stdin().lock().lines()
-        .map(|line| line.expect("Cannot read line"))
-        .collect();
+impl PositionAim {
+    fn new() -> Self {
+        PositionAim {
+            horizontal: 0,
+            depth: 0,
+            aim: 0,
+        }
+    }
 
+    fn forward(&mut self, delta: ValueType) {
+        self.horizontal += delta;
+        self.depth += self.aim * delta;
+    }
+
+    fn down(&mut self, delta: ValueType) {
+        self.aim += delta;
+    }
+
+    fn up(&mut self, delta: ValueType) {
+        self.aim -= delta;
+    }
+}
+
+fn part1(lines: Vec<String>) -> ValueType {
     let mut position = Position::new();
 
     for line in lines {
@@ -51,7 +77,45 @@ fn main() {
         };
     }
 
-    let result = position.horizontal * position.depth;
+    println!("position={0:?}", position);
 
-    println!("position={0:?} result={1:?}", position, result);
+    position.horizontal * position.depth
+}
+
+fn part2(lines: Vec<String>) -> ValueType {
+    let mut position = PositionAim::new();
+
+    for line in lines {
+        let mut tokens = line.split(" ");
+
+        let command = tokens.next().unwrap();
+        let delta: ValueType = tokens.next().map(|token| token.parse::<ValueType>()).unwrap().unwrap();
+
+        match command {
+            "forward" => position.forward(delta),
+            "up" => position.up(delta),
+            "down" => position.down(delta),
+            _ => panic!("Invalid command: {0:?}", command),
+        };
+    }
+
+    println!("position={0:?}", position);
+
+    position.horizontal * position.depth
+}
+
+fn main() {
+    let lines: Vec<String> = stdin().lock().lines()
+        .map(|line| line.expect("Cannot read line"))
+        .collect();
+
+    {
+        let result = part1(lines.clone());
+        println!("part1: result={0:?}", result);
+    }
+
+    {
+        let result = part2(lines.clone());
+        println!("part2: result={0:?}", result);
+    }
 }

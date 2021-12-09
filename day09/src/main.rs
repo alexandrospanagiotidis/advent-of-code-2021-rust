35778732a75f85d5923cf60db28b075f3e26e2c0
Alexandros Panagiotidis <alexandros@panagiotidis.eu>
2021-12-09 13:08:47 +0100

    Add day09 part 2

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,21 +1,26 @@
+use std::collections::{HashSet, VecDeque};
 use std::io::{BufRead, stdin};
+use std::ops::Range;
 
+type CoordType = i32;
 type ItemType = u32;
 
 #[derive(Debug)]
 struct Grid {
-    width: usize,
-    height: usize,
+    width: CoordType,
+    height: CoordType,
     heightmap: Vec<ItemType>,
+    horizontal: Range<CoordType>,
+    vertical: Range<CoordType>,
 }
 
 impl Grid {
     fn from_lines(lines: &Vec<String>) -> Self {
         let width = lines.iter().next()
             .expect("Could not read first line")
-            .len();
+            .len() as CoordType;
 
-        let height = lines.len();
+        let height = lines.len() as CoordType;
 
         let mut heightmap = Vec::new();
 
@@ -29,64 +34,76 @@ impl Grid {
             width,
             height,
             heightmap,
+            horizontal: 0..width,
+            vertical: 0..height,
         }
     }
 
-    fn at(&self, x: usize, y: usize) -> Option<&ItemType> {
-        self.heightmap.get(y * self.width + x)
+    fn at(&self, x: CoordType, y: CoordType) -> Option<&ItemType> {
+        self.heightmap.get((y * self.width + x) as usize)
     }
 
-    fn is_lowest(&self, x: usize, y: usize) -> bool {
+    fn neighbors_match(&self, x: CoordType, y: CoordType, predicate: impl Fn(&ItemType, CoordType, CoordType) -> bool) -> bool {
         let mid = self.at(x, y).expect("Could not determine mid");
 
-        if y > 0 {
-            if let Some(above) = self.at(x, y - 1) {
-                if above <= mid {
-                    return false;
-                }
-            }
-        }
-
-        if x > 0 {
-            if let Some(left) = self.at(x - 1, y) {
-                if left <= mid {
-                    return false;
-                }
-            }
-        }
-
-        if x < self.width - 1 {
-            if let Some(right) = self.at(x + 1, y) {
-                if right <= mid {
-                    return false;
-                }
-            }
-        }
-
-        if y < self.height - 1 {
-            if let Some(bottom) = self.at(x, y + 1) {
-                if bottom <= mid {
-                    return false;
-                }
+        for (x, y) in [(x, y - 1), (x - 1, y), (x + 1, y), (x, y + 1)] {
+            if self.is_inside(x, y) && !predicate(mid, x, y) {
+                return false;
             }
         }
 
         true
     }
 
-    fn all_lowest_points(&self) -> Vec<&ItemType> {
+    fn all_lowest_points(&self) -> Vec<(&ItemType, CoordType, CoordType)> {
         let mut lowest_points = Vec::new();
 
-        for row in 0..self.height {
-            for column in 0..self.width {
-                if self.is_lowest(column, row) {
-                    lowest_points.push(self.at(column, row)
-                        .expect("Could not get lowest point"));
+        for row in self.vertical.clone() {
+            for column in self.horizontal.clone() {
+                if self.neighbors_match(column, row, |mid, x, y| {
+                    self.at(x, y).map_or(false, |value| mid < value)
+                }) {
+                    let value = self.at(column, row)
+                        .expect("Could not get lowest point");
+                    lowest_points.push((value, column, row));
                 }
             }
         }
 
         lowest_points
+    }
+
+    fn is_inside(&self, x: CoordType, y: CoordType) -> bool {
+        self.horizontal.contains(&x) && self.vertical.contains(&y)
+    }
+
+    fn find_regions(&self, x: CoordType, y: CoordType, predicate: impl Fn(i32, i32) -> bool) -> Vec<&ItemType> {
+        let mut values = Vec::new();
+
+        let mut visited: HashSet<(CoordType, CoordType)> = HashSet::new();
+        visited.insert((x, y));
+
+        let mut seed_points = VecDeque::new();
+        seed_points.push_front((x, y));
+
+        while !seed_points.is_empty() {
+            let p = seed_points.pop_front()
+                .expect("Could not get next seed point");
+
+            let value = self.at(p.0, p.1)
+                .expect("Could not get mid point");
+
+            values.push(value);
+
+            for (x, y) in [(p.0, p.1 - 1 as CoordType), (p.0 - 1, p.1), (p.0 + 1, p.1), (p.0, p.1 + 1)] {
+                if !visited.contains(&(x, y)) && self.is_inside(x, y) && predicate(x, y) {
+                    seed_points.push_back((x, y));
+                    visited.insert((x, y));
+                }
+            }
+        }
+
+        values
     }
 }
 
@@ -99,9 +116,28 @@ fn main() {
     // println!("grid={0:?}", grid);
 
     let lowest_points = grid.all_lowest_points();
-    let risk_level: ItemType = lowest_points.into_iter()
-        .map(|&lowest_value| lowest_value + 1)
+    let risk_level: ItemType = lowest_points.iter()
+        .map(|low_point| low_point.0 + 1)
         .sum();
     println!("part1: risk_level={0:?}", risk_level);
     assert_eq!(risk_level, 500);
+
+    let mut basins = Vec::new();
+
+    for lowest_point in lowest_points.iter() {
+        basins.push(grid.find_regions(lowest_point.1, lowest_point.2, |x, y| {
+            grid.at(x, y).map_or(false, |value| value < &9)
+        }));
+    }
+
+    basins.sort_by(|a, b| (b.len().partial_cmp(&a.len())).unwrap());
+    // println!("basins={0:?}", basins);
+
+    let product_three_largest_basins = basins.iter()
+        .take(3)
+        .map(|basin| basin.len())
+        .map(|len| len)
+        .fold(1, |acc, value| acc * value);
+    println!("part2: product_three_largest_basins={0:?}", product_three_largest_basins);
+    assert_eq!(product_three_largest_basins, 970200);
 }

f3dd9533ead59a1a00ed0e83a966fb6f30fa5d07
Alexandros Panagiotidis <alexandros@panagiotidis.eu>
2021-12-10 11:32:09 +0100

    Add day10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -14,9 +14,10 @@ fn main() {
     valid_pairs.insert('<', '>');
 
     let mut illegal_char_count = HashMap::new();
+    let mut completed_scores = Vec::new();
 
-    for line in lines.iter() {
-        println!("scanning line={0:?}", line);
+    'next_line: for line in lines.iter() {
+        // println!("scanning line={0:?}", line);
 
         let mut this_line = VecDeque::new();
         this_line.reserve_exact(line.len());
@@ -24,7 +25,7 @@ fn main() {
         for token in line.chars() {
             // Opening characters go on the stack
             if valid_pairs.contains_key(&token) {
-                println!("found opening {0:?}", token);
+                // println!("found opening {0:?}", token);
                 this_line.push_front(token);
             } else {
                 // Otherwise we assume its the closing character, so check if it matches the top of stack
@@ -36,19 +37,46 @@ fn main() {
 
                 // Legal pairing
                 if &token == expected_closing {
-                    println!("found closing {0:?} (last_opening={1:?})", token, last_opening);
+                    // println!("found closing {0:?} (last_opening={1:?})", token, last_opening);
                     continue;
                 }
 
-                println!("{0:?} - Expected {1:?}, but found  {2:?} instead.", line, expected_closing, token);
+                // println!("{0:?} - Expected {1:?}, but found  {2:?} instead.", line, expected_closing, token);
 
                 *illegal_char_count.entry(token).or_insert(0) += 1;
-                break;
+                continue 'next_line;
             }
+        }
+
+        // Incomplete lines have missing closing chars
+        let mut completion_score: usize = 0;
+
+        for head in this_line.iter() {
+            let closing_char = valid_pairs.get(head)
+                .expect(format!("Could not determine closing char for head={0:?}", head).as_str());
+
+            let char_score = match closing_char {
+                ')' => 1,
+                ']' => 2,
+                '}' => 3,
+                '>' => 4,
+                _ => 0,
+            };
+
+            completion_score = completion_score * 5 + char_score;
+            // println!("closing with {0:?} for {1:?} points -> completion_score={2:?}", closing_char, char_score, completion_score);
+        }
+
+        if completion_score > 0 {
+            // println!("completing {0:?} - {1:?} total points.", this_line, completion_score);
+            completed_scores.push(completion_score);
         }
     }
 
     // println!("illegal_char_count={0:?}", illegal_char_count);
+
+    completed_scores.sort();
+    // println!("completed_scores={0:?}", completed_scores);
 
     let total_syntax_error_score: i32 = illegal_char_count.iter()
         .map(|(illegal_char, count)| {
@@ -62,4 +90,9 @@ fn main() {
         })
         .sum();
     println!("part1: total_syntax_error_score={0:?}", total_syntax_error_score);
+    assert_eq!(total_syntax_error_score, 240123);
+
+    let middle_score = completed_scores[completed_scores.len() / 2];
+    println!("part2: middle_score={0:?}", middle_score);
+    assert_eq!(middle_score, 3260812321);
 }

0d5b8b308147b6f931ca2edb07f35f4f009c71c1
Alexandros Panagiotidis <alexandros@panagiotidis.eu>
2021-12-12 18:51:55 +0100

    Add day12 part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,72 +1,133 @@
-use std::collections::{HashMap, HashSet};
+// Very heavily inspired by
+// https://github.com/ropewalker/advent_of_code_2021/blob/master/src/day12.rs
+// https://github.com/BigPeet/coding_challenges/blob/master/advent_of_code/2021/day12/src/lib.rs
+
+use std::borrow::Borrow;
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::io::{BufRead, stdin};
+use std::rc::Rc;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Node {
+    name: String,
+    small: bool,
+}
+
+impl Node {
+    fn new(s: &str) -> Node {
+        Node {
+            name: String::from(s),
+            small: s.to_lowercase() == s,
+        }
+    }
+}
 
 #[derive(Debug)]
 struct Graph {
-    connections: HashMap<String, HashSet<String>>,
+    connections: HashMap<Rc<Node>, HashSet<Rc<Node>>>,
 }
 
-fn parse_input(lines: Vec<String>) -> Graph {
-    let mut connections = HashMap::new();
-
-    for line in lines {
-        let mut tokens = line.split('-');
-
-        let source = tokens.next()
-            .expect(format!("Could not read source from: {0:?}", line).as_str());
-
-        let target = tokens.next()
-            .expect(format!("Could not read target from: {0:?}", line).as_str());
-
-        connections.entry(source.to_owned())
-            .or_insert_with(HashSet::new)
-            .insert(target.to_owned());
-
-        connections.entry(target.to_owned())
-            .or_insert_with(HashSet::new)
-            .insert(source.to_owned());
-    }
-
-    Graph {
-        connections
-    }
-}
-
-fn is_small(cave: &str) -> bool {
-    cave.to_ascii_lowercase() == *cave
-}
-
-fn simple_path_count(graph: &Graph, target: &str, current_node: &str, visits: &mut HashSet<String>) -> usize {
-    if current_node == target {
-        return 1;
-    }
-
-    let mut count = 0;
-
-    if let Some(neighbors) = graph.connections.get(current_node) {
-        for neighbor in neighbors {
-            if is_small(neighbor) {
-                if visits.contains(neighbor) {
-                    continue;
-                } else {
-                    visits.insert(neighbor.to_owned());
-                }
-            }
-
-            count += simple_path_count(graph, target, neighbor, visits);
-
-            visits.remove(neighbor);
+impl Graph {
+    fn new() -> Self {
+        Graph {
+            connections: HashMap::new(),
         }
     }
 
-    count
+    fn node(&self, name: &str) -> Option<&Node> {
+        self.connections.keys()
+            .find(|node| node.name == name)
+            .map(|node| node.borrow())
+    }
+
+    fn add_edge(&mut self, a: Node, b: Node) {
+        let a = Rc::new(a);
+        let b = Rc::new(b);
+
+        self.connections.entry(Rc::clone(&a))
+            .or_insert_with(HashSet::new)
+            .insert(Rc::clone(&b));
+
+        self.connections.entry(Rc::clone(&b))
+            .or_insert_with(HashSet::new)
+            .insert(Rc::clone(&a));
+    }
+
+    fn find_all_paths<'a>(&'a self, source: &'a Node, target: &'a Node, predicate: &impl Fn(&Node, &Path) -> bool) -> Vec<Path> {
+        let mut paths = Vec::new();
+        let mut boundary = VecDeque::new();
+
+        boundary.push_front((source, Path::new()));
+
+        while let Some((current_node, mut path)) = boundary.pop_front() {
+            path.add_node(current_node);
+
+            if current_node == target {
+                paths.push(path);
+            } else if let Some(neighbors) = self.connections.get(current_node) {
+                for valid_neighbor in neighbors.iter().filter(|neighbor| predicate(neighbor, &path)) {
+                    boundary.push_back((valid_neighbor, path.clone()));
+                }
+            }
+        }
+
+        paths
+    }
 }
 
-fn part1(graph: &Graph) -> usize {
-    let mut visits = HashSet::new();
-    visits.insert("start".to_owned());
+trait Day12 {
+    fn parse_input(lines: Vec<String>) -> Self;
+}
 
-    simple_path_count(graph, "end", "start", &mut visits)
+impl Day12 for Graph {
+    fn parse_input(lines: Vec<String>) -> Self {
+        let mut graph = Graph::new();
+
+        for line in lines {
+            let mut tokens = line.split('-');
+
+            let source = tokens.next()
+                .expect(format!("Could not read source from: {0:?}", line).as_str());
+
+            let target = tokens.next()
+                .expect(format!("Could not read target from: {0:?}", line).as_str());
+
+            graph.add_edge(Node::new(source), Node::new(target));
+        }
+
+        graph
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Path<'a> {
+    path: Vec<&'a Node>,
+    visited_twice: Option<&'a Node>,
+}
+
+impl<'a> Path<'a> {
+    fn new() -> Self {
+        Self {
+            path: Vec::new(),
+            visited_twice: None,
+        }
+    }
+
+    fn contains(&self, node: &Node) -> bool {
+        self.path.contains(&node)
+    }
+
+    fn count(&self, node: &Node) -> usize {
+        self.path.iter().filter(|&path_node| path_node == &node).count()
+    }
+
+    fn add_node(&mut self, node: &'a Node) {
+        if self.visited_twice.is_none() && node.small && self.contains(node) {
+            self.visited_twice = Some(node);
+        }
+
+        self.path.push(node);
+    }
 }
 
 fn main() {
@@ -74,9 +135,24 @@ fn main() {
         .map(|line| line.expect("Cannot read line"))
         .collect();
 
-    let graph: Graph = parse_input(lines);
-    println!("graph={0:?}", graph);
+    let graph: Graph = Day12::parse_input(lines);
+    // println!("graph={0:?}", graph);
 
-    let result = part1(&graph);
-    println!("result={0:?}", result);
+    let start = graph.node("start")
+        .expect("Could not find starting node");
+
+    let end = graph.node("end")
+        .expect("Could not find starting node");
+
+    let paths = graph.find_all_paths(start, end, &|node: &Node, path: &Path| {
+        !(node.small && path.contains(node))
+    });
+    println!("part1={0:?}", paths.len());
+    assert_eq!(paths.len(), 4691);
+
+    let paths = graph.find_all_paths(start, end, &|node: &Node, path: &Path| {
+        node != start && !(node.small && path.contains(node) && path.visited_twice.is_some())
+    });
+    println!("part2={0:?}", paths.len());
+    assert_eq!(paths.len(), 140718);
 }
